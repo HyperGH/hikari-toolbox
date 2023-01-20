d6b1dd2ca2ee67570256044d524a8404a7dede11
Lunarmagpie <bambolambo0@gmail.com>
2023-01-20 14:34:10 -0500

    add docs

--- toolbox/events.py
+++ toolbox/events.py
@@ -3,13 +3,32 @@ import typing as t
 import hikari
 import functools
 
+__all__: t.Sequence[str] = ["consume_event"]
+
 P = t.ParamSpec("P")
 T = t.TypeVar("T")
 
 
 def consume_event(callback: t.Callable[P, T]) -> t.Callable[t.Concatenate[hikari.Event, P], T]:
+    """
+    Consume the first argument of an event callback.
+
+    .. code-block:: python
+
+        import hikari
+        import toolbox
+
+        @toolbox.consume_event
+        async def on_started():
+            ...
+
+        bot = hikari.GatewayBot("TOKEN")
+        bot.subscribe(hikari.StartingEvent, on_started)
+        bot.run()
+    """
+
     @functools.wraps(callback)
-    def inner(_: hikari.Event, *args: P.args, **kwargs: P.kwargs) -> T:
-        return callback(*args, **kwargs)
+    async def inner(_: hikari.Event, *args: P.args, **kwargs: P.kwargs) -> T:
+        return await callback(*args, **kwargs)
 
     return inner
